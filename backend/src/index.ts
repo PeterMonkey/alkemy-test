d8d7d7f8ddabc3247ebad964bc785ca1ff3a6db4
Pedro Fernandez <pefernandez1408@gmail.com>
2022-04-29 21:49:30 -0400

    database configuration

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,16 @@
+import 'reflect-metadata';
 import app from './app';
+import {AppDataSource} from './db';
 
+function main(){
+    try{
+        AppDataSource.initialize()
+        app.listen(3000);
+        console.log('Server on port 3000')
+    }catch(error){
+        console.error(error)
+    }
+}
 
-app.listen(3000);
-console.log('Server on port 3000')
+main()
+
